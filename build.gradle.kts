6da94dcc2da3a060fbb7563ff9c942a7bed85f77
soft <soafftt@gmail.com>
2025-04-05 10:01:09 +0900

    feat: 프로젝트 구조 변경
    프로젝트의 모듈구조 및 모듈의 성향에 대한 README.md 작성.
    작업과정에서 변경이 될 수 있음.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,17 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "2.1.0"
-    kotlin("plugin.spring") version "2.1.0"
 }
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
+allprojects {
+    apply(plugin = "kotlin")
+
+    repositories {
+        mavenCentral()
+    }
 }
 
