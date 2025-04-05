a50a1c7c060c775be1692d0946bcc8e3b8529a49
soft <soafftt@gmail.com>
2025-04-05 09:28:17 +0900

    feat: 프로젝트 구조 변경
    프로젝트의 효율적 관리를 위하여, 각 모듈 분리 및 JVM 21, kotlin 2.1.0 으로 업그레이드
    
    구조
    commons
        - common
        ㄴ clients
            ㄴ cache
                - redis-lettuce
                - valkey-glide
            ㄴ http
                - ktor
                - webclient
            ㄴ db
                ㄴ r2dbc
                    - r2dbc-single
                    - r2dbc-multi

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "2.1.0"
+    kotlin("plugin.spring") version "2.1.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+}
+
